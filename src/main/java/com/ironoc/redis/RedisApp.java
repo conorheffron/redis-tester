56b2aaad72ece2f7ec6a2feecc903f029ea6ebe7
Conor Heffron <conor.heffron@gmail.com>
2017-01-26 12:29:45 +0000

    Initial commit of Jedis client for server check

---
+++ src/main/java/com/ironoc/redis/RedisApp.java
@@ -0,0 +1,48 @@
+package com.ironoc.redis;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
+
+public class RedisApp {
+
+	public static void main(String[] args) {
+		String serverUri = args[0];
+		String key = args[1];
+		String value = args[2];
+
+		JedisPool jedisPool = getConnection(serverUri);
+
+		Jedis jedis = jedisPool.getResource();
+		
+		// add token
+		jedis.set(key, value);
+		
+		// get token
+		String lookup = jedis.get(key);
+		System.out.println(lookup);
+		
+		// remove token
+		jedis.del(key);
+		System.out.println(jedis.get(key));
+		
+		// return the instance to the pool when you're done
+		jedisPool.returnResource(jedis);
+	}
+
+	private static JedisPool getConnection(String serverUri) {
+		try {
+			URI redisUri = new URI(serverUri);
+			JedisPool pool = new JedisPool(new JedisPoolConfig(), redisUri.getHost(), redisUri.getPort(),
+					Protocol.DEFAULT_TIMEOUT, redisUri.getUserInfo().split(":", 2)[1]);
+			return pool;
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
